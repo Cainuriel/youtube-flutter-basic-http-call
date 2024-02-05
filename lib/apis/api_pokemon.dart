6493e3e63da442ad32445c75e6683199410f9571
Fernando Lopez <cainuriel@gmail.com>
2024-02-05 20:07:21 +0100

    clean comments

--- lib/apis/api_pokemon.dart
+++ lib/apis/api_pokemon.dart
@@ -5,12 +5,10 @@ import '../../controllers/controller_pokemon.dart';
 class PokemonControllerImpl implements PokemonControler {
   @override
   Future<Pokemon?> getPokemon({int keyPokemon = 1}) async {
-    // await Future.delayed(const Duration(seconds: 1));
 
     final response =
         await Dio().get('https://pokeapi.co/api/v2/pokemon/$keyPokemon');
-    // final userAsString = await rootBundle.loadString('assets/jsons/user.json');
-    // final json = Map<String, dynamic>.from(jsonDecode(userAsString));
+
     return Pokemon.fromJson(response.data);
   }
 }
